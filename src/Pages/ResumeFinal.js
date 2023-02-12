70c50f5f865a3f10eec6ef0b43eec8da04cf3221
Nika Antauri <antaurinika2002@gmail.com>
2023-02-12 23:09:06 +0400

    added browser back button routing to last page

--- src/Pages/ResumeFinal.js
+++ src/Pages/ResumeFinal.js
@@ -7,6 +7,11 @@ import { FaAngleLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 export default function ResumeFinal() {
+  window.history.pushState(null, null, document.URL);
+  window.addEventListener("popstate", function (event) {
+    window.location.replace("/");
+    sessionStorage.clear();
+  });
   const navigate = useNavigate();
   return (
     <div>
